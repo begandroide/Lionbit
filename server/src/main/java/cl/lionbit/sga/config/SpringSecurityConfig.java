c4c510e863b167d3a7d11f673c4b9d0e44255f3e
Francisco Reyes <servoclay@gmail.com>
2019-07-16 13:00:29 -0400

    Se agregaron metodos para autenticación y se filtra la info. que se puede mostrar según el tipo de rol

--- server/src/main/java/cl/lionbit/sga/config/SpringSecurityConfig.java
+++ server/src/main/java/cl/lionbit/sga/config/SpringSecurityConfig.java
@@ -1,17 +1,28 @@
 package cl.lionbit.sga.config;
 
+import cl.lionbit.sga.error.CustomAccessDeniedHandler;
+import cl.lionbit.sga.security.AuthenticationSuccessHandler;
+import cl.lionbit.sga.security.RestAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@ComponentScan("cl.lionbit.sga.security")
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -19,13 +30,52 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
+    @Autowired
+    private CustomAccessDeniedHandler accessDeniedHandler;
+
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    private AuthenticationSuccessHandler SuccessHandler;
+
+    private SimpleUrlAuthenticationFailureHandler FailureHandler = new SimpleUrlAuthenticationFailureHandler();
+
+    public SpringSecurityConfig() {
+        super();
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
+
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/v2/api-docs").authenticated()
                 .and()
                 .httpBasic();
+    }*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler)
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/api/students/**").authenticated()
+                .antMatchers("/api/teachers/**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .successHandler(SuccessHandler)
+                .failureHandler(FailureHandler)
+                .and()
+                .httpBasic()
+                .and()
+                .logout();
     }
 
     @Autowired
@@ -36,6 +86,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // Creamos un user con su password (y se codicara la clave usando encoder) y roles
         builder.inMemoryAuthentication()
-                .withUser(users.username("havok").password("12345678").roles("ADMIN","USER"));
+                .withUser(users.username("admin").password("12345678").roles("ADMIN"))
+                .withUser(users.username("user").password("12345678").roles("USER"));
     }
 }
--- server/src/main/java/cl/lionbit/sga/constans/Paths.java
+++ server/src/main/java/cl/lionbit/sga/constans/Paths.java
@@ -2,8 +2,8 @@ package cl.lionbit.sga.constans;
 
 public class Paths {
     public static final String VERSION = "1.0";
-    public static final String STUDENTS = "students/";
-    public static final String TEACHERS = "teachers/";
+    public static final String STUDENTS = "/api/students/";
+    public static final String TEACHERS = "/api/teachers/";
     public static final String ROLES = "roles/";
     public static final String USERS = "users/";
 }
